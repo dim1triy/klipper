38a6861d217cb6264aef881dd2f891a419bfa6d5
Dmytro Ostrovtsov <dmytro.tech3d@gmail.com>
2024-05-02 15:06:34 +0000

    add test get_printer_config_mock

--- test/klippy/unit/extras/test_probe.py
+++ test/klippy/unit/extras/test_probe.py
@@ -1,20 +1,13 @@
 import unittest
-import reactor
-from configfile import PrinterConfig
 from extras.probe import ProbePointsHelper
-from klippy import Printer
-from test.lib import load_json
+from test.lib import get_printer_config_mock, load_json
 
 
 class ProbePointsHelperTest(unittest.TestCase):
 
     def test_check_position_value(self):
-        main_reactor = reactor.SelectReactor(gc_checking=False)
-        printer = Printer(main_reactor, None, {
-            'config_file': 'test/klippy/screws_tilt_adjust.cfg',
-        })
-        printer_config = PrinterConfig(printer)
-        config = printer_config.read_main_config()
+
+        config = get_printer_config_mock()
 
         test_cases = load_json('klippy/unit/extras/test_probe_cases.json')
 
--- test/lib.py
+++ test/lib.py
@@ -1,8 +1,24 @@
 import json
 import os
+import reactor
+from configfile import PrinterConfig
+from klippy import Printer
 
 
 def load_json(file_path):
     file = os.path.join(os.path.dirname(os.path.realpath(__file__)), file_path)
     with open(file, mode='r') as fp:
         return json.load(fp)
+
+
+def get_printer_config_mock(config_file=None):
+    if not config_file:
+        config_file = 'test/klippy/screws_tilt_adjust.cfg'
+
+    main_reactor = reactor.SelectReactor(gc_checking=False)
+    printer = Printer(main_reactor, None, {
+        'config_file': config_file,
+    })
+    printer_config = PrinterConfig(printer)
+
+    return printer_config.read_main_config()
